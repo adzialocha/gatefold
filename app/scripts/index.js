58e4d3540861d5183559de2317f619241c7a8441
Andreas Dzialocha <kontakt@andreasdzialocha.com>
2019-07-20 01:15:53 +0200

    Render components in view

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,3 +1,27 @@
+import { h, render } from 'preact';
+
+import components from './components';
+
 import '../styles/app.scss';
 
-console.log('Hello, World!');
+// Check for tags in DOM to render preact components
+const elems = document.querySelectorAll('[data-component]');
+
+[...elems].forEach(elem => {
+  const { dataset } = elem;
+
+  const name = Object.keys(components).find(component => {
+    return component === dataset.component;
+  });
+
+  if (name) {
+    // Render preact component
+    const WrapperComponent = components[name].default;
+
+    const props = ('props' in dataset) ? JSON.parse(dataset.props) : {};
+
+    render((<WrapperComponent {...props} />), elem);
+  } else {
+    throw new Error(`Could not find component with name "${dataset.component}"`);
+  }
+});
