43e9fb49c0c21792acd5597f4bbdee8fff5796ba
Andreas Dzialocha <kontakt@andreasdzialocha.com>
2019-07-20 01:14:43 +0200

    Remove unneeded splitChunk setting for webpack

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -6,7 +6,6 @@ import OptimizeCssAssetsPlugin from 'optimize-css-assets-webpack-plugin';
 import UglifyJsPlugin from 'uglifyjs-webpack-plugin';
 
 const APP_FILE_NAME = 'app';
-const VENDORS_FILE_NAME = 'lib';
 
 const PATH_SRC = './app';
 const PATH_DIST = './static';
@@ -81,15 +80,6 @@ export default (env, options) => {
         }),
         new OptimizeCssAssetsPlugin(),
       ],
-      splitChunks: {
-        cacheGroups: {
-          commons: {
-            test: new RegExp(NODE_MODULES_NAME),
-            chunks: 'all',
-            name: VENDORS_FILE_NAME,
-          },
-        },
-      },
     },
     devtool: isDevelopment ? 'cheap-module-source-map' : undefined,
     plugins: [
