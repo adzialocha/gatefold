a4e074a384aacf75c7756595e5f3c2e1b3c0722c
Andreas Dzialocha <kontakt@andreasdzialocha.com>
2019-07-21 00:52:10 +0200

    Fix: Make city field in table airports larger

--- server/db/migrations/20190706222442_airports.js
+++ server/db/migrations/20190706222442_airports.js
@@ -3,7 +3,7 @@ exports.up = db => {
     t.increments('id').primary();
     t.timestamps(false, true);
     t.string('iata', 3);
-    t.string('country', 2);
+    t.string('country', 255);
     t.string('name', 255);
     t.string('lat', 32);
     t.string('lon', 32);
