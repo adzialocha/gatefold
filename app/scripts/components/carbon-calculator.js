3a2c22680ce81afc2aa4c6af4b1b816985b83ca8
Andreas Dzialocha <kontakt@andreasdzialocha.com>
2019-07-20 19:39:26 +0200

    Calculate carbon emission and offset costs

--- app/scripts/components/carbon-calculator.js
+++ app/scripts/components/carbon-calculator.js
@@ -3,29 +3,69 @@ import { h, Component } from 'preact';
 
 import AirportSelector from './airport-selector';
 
+import {
+  calculateCosts,
+  calculateDistance,
+  calculateEmission,
+} from '../../../common/carbon-calculator';
+
 export default class CarbonCalculator extends Component {
-  componentDidUpdate(prevProps, prevState) {
-    const { from, to } = this.state.airports;
-
-    if (from && to) {
-      this.calculateCarbonOffset(from, to);
-    }
-  }
-
   onAirportChange(event) {
     const { name, value } = event;
 
-    this.setState({
-      airports: Object.assign({}, this.state.airports, {
-        [name]: value,
-      }),
+    this._airports = Object.assign({}, this._airports, {
+      [name]: value,
     });
 
-    if (!value) {
+    const { from, to } = this._airports;
+
+    if (from && to) {
+      this.calculateCarbonOffset(from, to);
+    } else {
       this.setState({
         calculation: undefined,
       });
     }
+  }
+
+  renderAirports() {
+    return (
+      <div className='carbon-calculator__airports'>
+        <label for='from'>
+          From airport
+          <AirportSelector name='from' value={this.props.from} onChange={this.onAirportChange} />
+        </label>
+
+        <label for='to'>
+          To airport
+          <AirportSelector name='to' value={this.props.to} onChange={this.onAirportChange} />
+        </label>
+      </div>
+    );
+  }
+
+  renderCalculation() {
+    if (!this.state.calculation) {
+      return;
+    }
+
+    const { distance, costs, emission } = this.state.calculation;
+
+    return (
+      <div className='carbon-calculator__results'>
+        <div className='carbon-calculator__results-distance'>
+          { distance } Kilometers
+        </div>
+
+        <div className='carbon-calculator__results-emission'>
+          { emission } Kilogram CO2
+        </div>
+
+        <div className='carbon-calculator__results-costs'>
+          { costs } Euro
+        </div>
+      </div>
+    );
   }
 
   render() {
@@ -33,33 +73,35 @@ export default class CarbonCalculator extends Component {
 
     return (
       <div className={className}>
-        <div className='carbon-calculator__airports'>
-          <label for='from'>
-            From airport
-            <AirportSelector name='from' value={this.props.from} onChange={this.onAirportChange} />
-          </label>
-
-          <label for='to'>
-            To airport
-            <AirportSelector name='to' value={this.props.to} onChange={this.onAirportChange} />
-          </label>
-        </div>
+        { this.renderAirports() }
+        { this.renderCalculation() }
       </div>
     );
   }
 
   calculateCarbonOffset(from, to) {
-    // @TODO
+    const distance = calculateDistance(from.lat, from.lon, to.lat, to.lon);
+    const emission = calculateEmission(distance);
+    const costs = calculateCosts(emission);
+
+    this.setState({
+      calculation: {
+        costs: costs.toFixed(2),
+        distance: distance.toFixed(2),
+        emission: emission.toFixed(2),
+      },
+    });
   }
 
   constructor(props) {
     super(props);
 
+    this._airports = {
+      from: undefined,
+      to: undefined,
+    };
+
     this.state = {
-      airports: {
-        from: undefined,
-        to: undefined,
-      },
       calculation: undefined,
     };
 
---
+++ common/carbon-calculator.js
@@ -0,0 +1,35 @@
+const ABSORPTION_TREE_PER_YEAR = 21.77;
+const CARBON_KG_PER_KILOMETER = 0.158;
+const COSTS_PER_TREE = 1;
+const EARTH_RADIUS = 6371;
+
+function deg2rad(deg) {
+  return deg * (Math.PI / 180);
+}
+
+function calculateDistance(lat1, lon1, lat2, lon2) {
+  const dLat = deg2rad(lat2 - lat1);
+  const dLon = deg2rad(lon2 - lon1);
+
+  const a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
+    Math.sin(dLon / 2) * Math.sin(dLon / 2);
+
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+  return EARTH_RADIUS * c;
+}
+
+function calculateEmission(distance) {
+  return distance * CARBON_KG_PER_KILOMETER;
+}
+
+function calculateCosts(emission) {
+  return (emission / ABSORPTION_TREE_PER_YEAR) * COSTS_PER_TREE;
+}
+
+module.exports = {
+  calculateCosts,
+  calculateDistance,
+  calculateEmission,
+};
