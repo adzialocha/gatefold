f45ffb42b77d612cdef4a5a6e1268561bdaaeb14
Andreas Dzialocha <kontakt@andreasdzialocha.com>
2019-07-21 18:39:19 +0200

    Double distance because its a round-trip for now

--- common/carbon-calculator.js
+++ common/carbon-calculator.js
@@ -17,14 +17,17 @@ function calculateDistance(lat1, lon1, lat2, lon2) {
 
   const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
 
-  return EARTH_RADIUS * c;
+  // @TODO: Give option to have oneway or round trip flights
+  return EARTH_RADIUS * c * 2; // Round trip!
 }
 
 function calculateEmission(distance) {
+  // @TODO: Use better formula
   return distance * CARBON_KG_PER_KILOMETER;
 }
 
 function calculateCosts(emission) {
+  // @TODO: Use better formula
   return (emission / ABSORPTION_TREE_PER_YEAR) * COSTS_PER_TREE;
 }
 
